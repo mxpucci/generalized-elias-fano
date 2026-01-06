4af3a131ad91680755b9b9e6bf198ac5895288ff
mxpucci <michelangelopucci.mp@gmail.com>
2026-01-06 15:28:59 +0100

    Remove leftovers

--- benchmarks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/generalized-elias-fano/benchmarks
-# Build directory: /root/generalized-elias-fano/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test_simd.cpp
+++
@@ -1,8 +0,0 @@
-#include <experimental/simd>
-#include <iostream>
-namespace stdx = std::experimental;
-int main() {
-    using V = stdx::native_simd<int>;
-    std::cout << V::size() << std::endl;
-    return 0;
-}
